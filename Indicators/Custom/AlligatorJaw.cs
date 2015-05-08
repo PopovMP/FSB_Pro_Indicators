98a4be23b822f7ac05da29f1cf7cab852e540c1d
Miroslav Popov <info@forexsb.com>
2015-05-08 14:51:21 +0300

    Added Alligator Jaw indicator.

---
+++ Indicators/Custom/AlligatorJaw.cs
@@ -0,0 +1,52 @@
+﻿//==============================================================
+// Forex Strategy Builder
+// Copyright © Miroslav Popov. All rights reserved.
+//==============================================================
+// THIS CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE.
+//==============================================================
+
+using System;
+using System.Drawing;
+using ForexStrategyBuilder.Infrastructure.Enums;
+using ForexStrategyBuilder.Infrastructure.Interfaces;
+
+namespace ForexStrategyBuilder.Indicators.Custom
+{
+    public class AlligatorJaw : MainChartSingleLineIndicator
+    {
+        public AlligatorJaw()
+        {
+            IndicatorName        = "Alligator Jaw";
+            IndicatorVersion     = "1.0";
+            IndicatorAuthor      = "Miroslav Popov";
+            IndicatorDescription = "Bill Williams Alligator Jaw";
+        }
+
+        public override void Initialize(SlotTypes slotType)
+        {
+            base.Initialize(slotType);
+
+            // List parameters
+            IndParam.ListParam[1].Index = (int) MAMethod.Smoothed;
+            IndParam.ListParam[1].Text  = MAMethod.Smoothed.ToString();
+
+            // Adds two numeric parameters more
+            IndParam.NumParam[0].Value = 15; // Deafult period
+            IndParam.NumParam[1].Value = 8;  // Default shift
+        }
+
+        public override void Calculate(IDataSet dataSet)
+        {
+            InitCalculation(dataSet);
+
+            IndicatorLine = MovingAverage(IndicatorPeriod, IndicatorShift, IndicatorMaMethod, Price(IndicatorBasePrice));
+
+            PostCalculation();
+
+            Component[0].ChartColor = Color.Blue; // Alligator Jaw is Blue
+        }
+    }
+}
