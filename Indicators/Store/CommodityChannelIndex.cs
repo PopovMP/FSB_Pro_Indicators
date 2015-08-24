367c4210f5a69d6e819d4646aa5757c153438444
Miroslav Popov <info@forexsb.com>
2015-08-24 09:53:19 +0300

    CCI code fixed.

--- Indicators/Store/CommodityChannelIndex.cs
+++ Indicators/Store/CommodityChannelIndex.cs
@@ -1,6 +1,6 @@
 //==============================================================
 // Forex Strategy Builder
-// Copyright © Miroslav Popov. All rights reserved.
+// Copyright © Forex Software Ltd. All rights reserved.
 //==============================================================
 // THIS CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
 // EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
@@ -25,7 +25,7 @@ namespace ForexStrategyBuilder.Indicators.Store
             SeparatedChart = true;
 
             IndicatorAuthor = "Miroslav Popov";
-            IndicatorVersion = "2.0";
+            IndicatorVersion = "2.1";
             IndicatorDescription = "Bundled in FSB distribution.";
         }
 
@@ -38,16 +38,16 @@ namespace ForexStrategyBuilder.Indicators.Store
             // The ComboBox parameters
             IndParam.ListParam[0].Caption = "Logic";
             IndParam.ListParam[0].ItemList = new[]
-                {
-                    "CCI rises",
-                    "CCI falls",
-                    "CCI is higher than the Level line",
-                    "CCI is lower than the Level line",
-                    "CCI crosses the Level line upward",
-                    "CCI crosses the Level line downward",
-                    "CCI changes its direction upward",
-                    "CCI changes its direction downward"
-                };
+            {
+                "CCI rises",
+                "CCI falls",
+                "CCI is higher than the Level line",
+                "CCI is lower than the Level line",
+                "CCI crosses the Level line upward",
+                "CCI crosses the Level line downward",
+                "CCI changes its direction upward",
+                "CCI changes its direction downward"
+            };
             IndParam.ListParam[0].Index = 0;
             IndParam.ListParam[0].Text = IndParam.ListParam[0].ItemList[IndParam.ListParam[0].Index];
             IndParam.ListParam[0].Enabled = true;
@@ -101,64 +101,63 @@ namespace ForexStrategyBuilder.Indicators.Store
             DataSet = dataSet;
 
             // Reading the parameters
+            var maMethod = (MAMethod) IndParam.ListParam[1].Index;
             var basePrice = (BasePrice) IndParam.ListParam[2].Index;
-            var iPeriod = (int) IndParam.NumParam[0].Value;
-            double dLevel = IndParam.NumParam[1].Value;
-            double dMultiplier = IndParam.NumParam[2].Value;
-            int iPrvs = IndParam.CheckParam[0].Checked ? 1 : 0;
+            var period = (int) IndParam.NumParam[0].Value;
+            double level = IndParam.NumParam[1].Value;
+            double multiplier = IndParam.NumParam[2].Value;
+            int previous = IndParam.CheckParam[0].Checked ? 1 : 0;
 
             // Calculation
-            int iFirstBar = iPeriod + 3;
-            double[] adBasePrice = Price(basePrice);
-            double[] adSmaBasePrice = MovingAverage(iPeriod, 0, MAMethod.Simple, adBasePrice);
+            int firstBar = period + 3;
+            double[] price = Price(basePrice);
+            double[] movingAverage = MovingAverage(period, 0, maMethod, price);
 
-            var adMeanDev = new double[Bars];
-            for (int iBar = iPeriod; iBar < Bars; iBar++)
+            var meanDev = new double[Bars];
+            for (int bar = period; bar < Bars; bar++)
             {
                 double sum = 0;
-                for (int i = 0; i < iPeriod; i++)
-                {
-                    sum += Math.Abs(adBasePrice[iBar - i] - adSmaBasePrice[iBar]);
-                }
-                adMeanDev[iBar] = dMultiplier*sum/iPeriod;
+                for (int i = 0; i < period; i++)
+                    sum += Math.Abs(price[bar - i] - movingAverage[bar]);
+                meanDev[bar] = multiplier*sum/period;
             }
 
-            var adCci = new double[Bars];
+            var cci = new double[Bars];
 
-            for (int iBar = iFirstBar; iBar < Bars; iBar++)
+            for (int bar = firstBar; bar < Bars; bar++)
             {
-                if (Math.Abs(adMeanDev[iBar] - 0) > Epsilon)
-                    adCci[iBar] = (adBasePrice[iBar] - adSmaBasePrice[iBar])/adMeanDev[iBar];
+                if (Math.Abs(meanDev[bar] - 0) > Epsilon)
+                    cci[bar] = (price[bar] - movingAverage[bar])/meanDev[bar];
                 else
-                    adCci[iBar] = 0;
+                    cci[bar] = 0;
             }
 
             // Saving the components
             Component = new IndicatorComp[3];
 
             Component[0] = new IndicatorComp
-                {
-                    CompName = "CCI",
-                    DataType = IndComponentType.IndicatorValue,
-                    ChartType = IndChartType.Line,
-                    ChartColor = Color.RoyalBlue,
-                    FirstBar = iFirstBar,
-                    Value = adCci
-                };
+            {
+                CompName = "CCI",
+                DataType = IndComponentType.IndicatorValue,
+                ChartType = IndChartType.Line,
+                ChartColor = Color.RoyalBlue,
+                FirstBar = firstBar,
+                Value = cci
+            };
 
             Component[1] = new IndicatorComp
-                {
-                    ChartType = IndChartType.NoChart,
-                    FirstBar = iFirstBar,
-                    Value = new double[Bars]
-                };
+            {
+                ChartType = IndChartType.NoChart,
+                FirstBar = firstBar,
+                Value = new double[Bars]
+            };
 
             Component[2] = new IndicatorComp
-                {
-                    ChartType = IndChartType.NoChart,
-                    FirstBar = iFirstBar,
-                    Value = new double[Bars]
-                };
+            {
+                ChartType = IndChartType.NoChart,
+                FirstBar = firstBar,
+                Value = new double[Bars]
+            };
 
             // Sets the Component's type.
             if (SlotType == SlotTypes.OpenFilter)
@@ -193,22 +192,22 @@ namespace ForexStrategyBuilder.Indicators.Store
 
                 case "CCI is higher than the Level line":
                     indLogic = IndicatorLogic.The_indicator_is_higher_than_the_level_line;
-                    SpecialValues = new[] {dLevel, 0, - dLevel};
+                    SpecialValues = new[] {level, 0, -level};
                     break;
 
                 case "CCI is lower than the Level line":
                     indLogic = IndicatorLogic.The_indicator_is_lower_than_the_level_line;
-                    SpecialValues = new[] {dLevel, 0, -dLevel};
+                    SpecialValues = new[] {level, 0, -level};
                     break;
 
                 case "CCI crosses the Level line upward":
                     indLogic = IndicatorLogic.The_indicator_crosses_the_level_line_upward;
-                    SpecialValues = new[] {dLevel, 0, -dLevel};
+                    SpecialValues = new[] {level, 0, -level};
                     break;
 
                 case "CCI crosses the Level line downward":
                     indLogic = IndicatorLogic.The_indicator_crosses_the_level_line_downward;
-                    SpecialValues = new[] {dLevel, 0, -dLevel};
+                    SpecialValues = new[] {level, 0, -level};
                     break;
 
                 case "CCI changes its direction upward":
@@ -222,17 +221,17 @@ namespace ForexStrategyBuilder.Indicators.Store
                     break;
             }
 
-            OscillatorLogic(iFirstBar, iPrvs, adCci, dLevel, - dLevel, ref Component[1], ref Component[2], indLogic);
+            OscillatorLogic(firstBar, previous, cci, level, -level, ref Component[1], ref Component[2], indLogic);
         }
 
         public override void SetDescription()
         {
-            string sLevelLong = (Math.Abs(IndParam.NumParam[1].Value - 0) < Epsilon
-                                     ? "0"
-                                     : IndParam.NumParam[1].ValueToString);
-            string sLevelShort = (Math.Abs(IndParam.NumParam[1].Value - 0) < Epsilon
-                                      ? "0"
-                                      : "-" + IndParam.NumParam[1].ValueToString);
+            string levelLong = (Math.Abs(IndParam.NumParam[1].Value - 0) < Epsilon
+                ? "0"
+                : IndParam.NumParam[1].ValueToString);
+            string levelShort = (Math.Abs(IndParam.NumParam[1].Value - 0) < Epsilon
+                ? "0"
+                : "-" + IndParam.NumParam[1].ValueToString);
 
             EntryFilterLongDescription = ToString() + " ";
             EntryFilterShortDescription = ToString() + " ";
@@ -256,31 +255,31 @@ namespace ForexStrategyBuilder.Indicators.Store
                     break;
 
                 case "CCI is higher than the Level line":
-                    EntryFilterLongDescription += "is higher than the Level " + sLevelLong;
-                    EntryFilterShortDescription += "is lower than the Level " + sLevelShort;
-                    ExitFilterLongDescription += "is higher than the Level " + sLevelLong;
-                    ExitFilterShortDescription += "is lower than the Level " + sLevelShort;
+                    EntryFilterLongDescription += "is higher than the Level " + levelLong;
+                    EntryFilterShortDescription += "is lower than the Level " + levelShort;
+                    ExitFilterLongDescription += "is higher than the Level " + levelLong;
+                    ExitFilterShortDescription += "is lower than the Level " + levelShort;
                     break;
 
                 case "CCI is lower than the Level line":
-                    EntryFilterLongDescription += "is lower than the Level " + sLevelLong;
-                    EntryFilterShortDescription += "is higher than the Level " + sLevelShort;
-                    ExitFilterLongDescription += "is lower than the Level " + sLevelLong;
-                    ExitFilterShortDescription += "is higher than the Level " + sLevelShort;
+                    EntryFilterLongDescription += "is lower than the Level " + levelLong;
+                    EntryFilterShortDescription += "is higher than the Level " + levelShort;
+                    ExitFilterLongDescription += "is lower than the Level " + levelLong;
+                    ExitFilterShortDescription += "is higher than the Level " + levelShort;
                     break;
 
                 case "CCI crosses the Level line upward":
-                    EntryFilterLongDescription += "crosses the Level " + sLevelLong + " upward";
-                    EntryFilterShortDescription += "crosses the Level " + sLevelShort + " downward";
-                    ExitFilterLongDescription += "crosses the Level " + sLevelLong + " upward";
-                    ExitFilterShortDescription += "crosses the Level " + sLevelShort + " downward";
+                    EntryFilterLongDescription += "crosses the Level " + levelLong + " upward";
+                    EntryFilterShortDescription += "crosses the Level " + levelShort + " downward";
+                    ExitFilterLongDescription += "crosses the Level " + levelLong + " upward";
+                    ExitFilterShortDescription += "crosses the Level " + levelShort + " downward";
                     break;
 
                 case "CCI crosses the Level line downward":
-                    EntryFilterLongDescription += "crosses the Level " + sLevelLong + " downward";
-                    EntryFilterShortDescription += "crosses the Level " + sLevelShort + " upward";
-                    ExitFilterLongDescription += "crosses the Level " + sLevelLong + " downward";
-                    ExitFilterShortDescription += "crosses the Level " + sLevelShort + " upward";
+                    EntryFilterLongDescription += "crosses the Level " + levelLong + " downward";
+                    EntryFilterShortDescription += "crosses the Level " + levelShort + " upward";
+                    ExitFilterLongDescription += "crosses the Level " + levelLong + " downward";
+                    ExitFilterShortDescription += "crosses the Level " + levelShort + " upward";
                     break;
 
                 case "CCI changes its direction upward":
